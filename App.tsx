0620270ddfb520f14892a556586f9bf7fadf90f4
Mateus Lopes <mateusbslopes@gmail.com>
2020-07-22 15:18:23 -0300

    feat: Config redux on app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
 import Main from './src/Main'
+import store from './src/store/ducks'
+import { Provider } from 'react-redux'
+
+export { store }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Main />
-    </View>
+      <Provider store={store}>
+        <Main />
+      </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text } from 'react-native'
+import Form from './components/pages/Form'
 
 
 export default class Main extends Component {
 
     render() {
         return (
-            <View>
-                <Text>
-                    teste
-                </Text>
+            <View style={styles.container}>
+                <Form  />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
